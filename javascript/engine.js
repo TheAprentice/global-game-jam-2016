3e930a1e5f11b93bd7ddfb2030acd68450844bcf
TheAprentice <gas_23@live.com>
2016-01-30 16:52:19 -0500

    Added sound to original sound manager

--- javascript/engine.js
+++ javascript/engine.js
@@ -1,18 +1,18 @@
 function Engine() {
 	//Gentlemen, start your engines!
 	this.start();
-	
+
 	return;
 }
 
 Engine.prototype.start = function() {
 	setInterval(this.loop, 0);
-	
+
 	return;
 }
 
 Engine.prototype.loop = function() {
-	
-	
+
+
 	return;
 }
--- javascript/sound_manager.js
+++ javascript/sound_manager.js
@@ -1,11 +1,41 @@
 Engine.prototype.SoundManager = (function() {
 	function SoundManager() {
 		console.log("SoundManager loaded");
-		
+
+		this.audioElement = document.createElement("audio");
+
+		//comment this.text() for production!
+		this.test();
+
 		return;
 	}
-	
-	//SoundManager.prototype.whatever = function(){/* definition here */}
-	
+
+	SoundManager.prototype.playAudio = function(audioProperties) {
+		this.audioElement.src = audioProperties.source;
+		this.audioElement.volume = audioProperties.volume;
+		//this.audioElement.other_properties = audioProperties.other_properties;
+
+		return this.audioElement.play();
+	}
+
+	SoundManager.prototype.test = function() {
+		//test whatever you want here =)
+
+		//WARNING: actual game object code is subject to change and defined in game logic
+		var coin = {
+			//position: {x: 0, y: 0, z:0},
+			//ObjectType: "coin",
+			//...
+			audioProperties: {
+				source: "assets/coin_sound.wav",
+				volume: 0.4
+			}
+		}
+
+		this.playAudio(coin.audioProperties);
+
+		return;
+	}
+
 	return new SoundManager();
 })();
