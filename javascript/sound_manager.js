901ec44af25af7ec8392e34efea8117ec9aa442d
TheAprentice <gas_23@live.com>
2016-01-30 18:44:27 -0500

    modified soundmanager

--- javascript/sound_manager.js
+++ javascript/sound_manager.js
@@ -4,6 +4,7 @@ Engine.prototype.SoundManager = (function() {
 
 		this.audioElement = document.createElement("audio");
 
+		this.bufferLoader = this.loadBuffer();
 		//comment this.text() for production!
 		this.test();
 
@@ -18,21 +19,47 @@ Engine.prototype.SoundManager = (function() {
 		return this.audioElement.play();
 	}
 
+	SoundManager.prototype.loadBuffer = function(){
+		var bufferLoader =  [
+					"assets/coin_sound.wav",
+					"assets/gather_sound.mp3",
+					"assets/anvil.wav",
+					"assets/clang.wav",
+					"assets/low_bell.wav",
+					"assets/monster.wav",
+					"assets/pan_dong.wav",
+					"assets/pan_hit.wav",
+					"assets.tinkling.wav",
+					"assets/trainsound.wav"
+				];
+
+		return bufferLoader;
+	}
+
 	SoundManager.prototype.test = function() {
 		//test whatever you want here =)
 
 		//WARNING: actual game object code is subject to change and defined in game logic
-		var coin = {
+		/*var coin = {
 			//position: {x: 0, y: 0, z:0},
 			//ObjectType: "coin",
 			//...
 			audioProperties: {
-				source: "assets/coin_sound.wav",
+				source: this.bufferLoader[8],
 				volume: 0.4
+			}
+		}*/
+
+		var monster = {
+			audioProperties: {
+				source: this.bufferLoader[5],
+				volume: 1
 			}
 		}
 
-		this.playAudio(coin.audioProperties);
+
+		//this.playAudio(coin.audioProperties);
+		this.playAudio(monster.audioProperties);
 
 		return;
 	}
