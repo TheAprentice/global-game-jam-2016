a49750b98307ae3310ae1e04c1d8c1d6d800610a
LAN Default <lfg@LFG.LOCAL>
2016-01-29 21:37:02 -0500

    First commit

---
+++ javascript/engine.js
@@ -0,0 +1,20 @@
+(function Engine() {
+    function InputManager() {
+        console.log("InputManager loaded");
+        
+        return;
+    }
+    
+    function SoundManager() {
+        console.log("SoundManager loaded");
+        
+        return;
+    }
+    
+    var inputManager = new InputManager(),
+        soundManager = new SoundManager();
+    
+    //
+    
+    return;
+})();
