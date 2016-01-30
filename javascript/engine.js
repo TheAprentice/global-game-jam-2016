d333afb0aba20a0a62f5409b39d32b3c9aa262b8
LAN Default <lfg@LFG.LOCAL>
2016-01-30 17:39:27 -0500

    Engine update

--- javascript/engine.js
+++ javascript/engine.js
@@ -1,18 +1,25 @@
 function Engine() {
-	//Gentlemen, start your engines!
-	this.start();
+	this.intervalID;
 	
 	return;
 }
 
 Engine.prototype.start = function() {
-	setInterval(this.loop, 0);
+	var thisEngine = this;
+	
+	this.intervalID = setInterval(function() {
+		return thisEngine.loop();
+	}, 0);
+	
+console.log(this);
 	
 	return;
 }
 
+//TODO: DEFINE THIS IN GAME LOGIC
 Engine.prototype.loop = function() {
-	
+	console.log("Please define Engine.prototype.loop()");
+	clearInterval(this.intervalID);
 	
 	return;
 }
